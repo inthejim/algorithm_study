37eb458ab25f419e1701ff65da017a7e784cee4f
inthejim <107672313+inthejim@users.noreply.github.com>
2023-08-15 07:55:05 +0900

    Update 백준2194.py

--- 백준2194.py
+++ 백준2194.py
@@ -2,13 +2,13 @@ from collections import deque
 import sys
 
 data=[]
-global n,m,a,b
 n,m,a,b,k=map(int,input().split())
-global map
-map=[[0]*m for _ in range(n)]
+
+mp=[[0]*m for _ in range(n)]
 for _ in range(k):
     x,y=map(int,sys.stdin.readline().split())
-    map[x][y]=2
+    mp[x-1][y-1]=2
+
 sx,sy=map(int,input().split())
 d_x,d_y=map(int,input().split())
 
@@ -16,20 +16,29 @@ dx=[1,-1,0,0]
 dy=[0,0,1,-1]
 visited=[[0]*m for _ in range(n)]
 
+def check(sx,sy,d_x,d_y):
+    for i in range(sx,sy):
+        for j in range(d_x,d_y):
+            if(mp[i][j]==2):
+                return 0
+    return 1
+
 def bfs(sx,sy,d_x,d_y):
-    visited[sx][sy]=1
+    visited[sx-1][sy-1]=1
     queue=deque([])
-    queue.append([sx,sy,0])
+    queue.append([sx-1,sy-1,0])
     while(queue):
         x,y,dis=queue.pop()
-        if(x==dx and y==dy):
+        if(x==d_x-1 and y==d_y-1):
             return dis
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
-            if(visited[nx][ny]==0 and 0<=nx and 0<=ny and nx+a<n and ny+b<m):
-                visited[nx][ny]=1
-                queue.append([nx,ny,dis+1])
+            if(0<=nx and 0<=ny and nx+b<n and ny+a<m and visited[nx][ny]==0):
+                #장애물 체크
+                if(check):
+                    visited[nx][ny]=1
+                    queue.append([nx,ny,dis+1])
     return -1
 
 
