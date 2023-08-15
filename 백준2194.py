0f328c3742abb25c932f300d3d11dedec45ea774
inthejim <107672313+inthejim@users.noreply.github.com>
2023-08-15 18:14:42 +0900

    Update 백준2194.py

--- 백준2194.py
+++ 백준2194.py
@@ -14,32 +14,34 @@ d_x,d_y=map(int,input().split())
 
 dx=[1,-1,0,0]
 dy=[0,0,1,-1]
+
 visited=[[0]*m for _ in range(n)]
 
-def check(sx,sy,d_x,d_y):
-    for i in range(sx,sy):
-        for j in range(d_x,d_y):
+def check(ix,iy,ox,oy):
+    for i in range(ix,ox):
+        for j in range(iy,oy):
             if(mp[i][j]==2):
                 return 0
     return 1
 
-def bfs(sx,sy,d_x,d_y):
+def bfs():
     visited[sx-1][sy-1]=1
     queue=deque([])
     queue.append([sx-1,sy-1,0])
+
     while(queue):
-        x,y,dis=queue.pop()
+        x,y,dis=queue.popleft()
+
         if(x==d_x-1 and y==d_y-1):
             return dis
+        
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
-            if(0<=nx and 0<=ny and nx+b<n and ny+a<m and visited[nx][ny]==0):
-                #장애물 체크
-                if(check):
+            if(0<=nx and 0<=ny and (nx+a-1)<n and (ny+b-1)<m and visited[nx][ny]==0):
+                if(check(nx,ny,nx+a,ny+b)):
                     visited[nx][ny]=1
                     queue.append([nx,ny,dis+1])
     return -1
 
-
-print(bfs(sx,sy,d_x,d_y))
+print(bfs())
