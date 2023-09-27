3b86dc231593258b1198ef60403aee8bc371e901
inthejim <107672313+inthejim@users.noreply.github.com>
2023-09-27 23:58:52 +0900

    Create 백준_1654.py

---
+++ 백준_1654.py
@@ -0,0 +1,19 @@
+import sys
+si=sys.stdin.readline
+n, k=map(int, si().split(' '))
+lines=[int(si()) for _ in range(n)]
+
+start, end=1, max(lines)
+
+while(start<end):
+    mid=(start+end)//2
+    count=0
+    for line in lines:
+        count+=line//mid
+    
+    if count>=n:
+        start=mid+1
+    else:
+        end=mid-1
+
+print(end)
